8faa219bc61efb00cebc2e70f8838da475a4669f
Aurélien Pupier <apupier@redhat.com>
2019-01-23 10:16:29 +0100

    Provide test for occupied port when starting AtlasMap #18
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>

--- src/test/command.test.ts
+++ src/test/command.test.ts
@@ -1,5 +1,6 @@
 "use strict";
 
+import * as assert from "assert";
 import * as vscode from "vscode";
 import * as chai from "chai";
 import * as sinonChai from "sinon-chai";
@@ -49,6 +50,36 @@ describe("AtlasMap/Commands", () => {
 
 		it("works with valid inputs", async () => {
 			await vscode.commands.executeCommand("atlasmap.start");
+		});
+	});
+
+	describe("Start", () => {
+
+		let port = "8586";
+		let errorMessageSpy = sandbox.spy(vscode.window, "showErrorMessage");
+
+		before(() => {
+			inputStub.onFirstCall().returns(port);
+			inputStub.onSecondCall().returns(port);
+		});
+
+		after(() => {
+			inputStub.reset();
+		});
+
+		it("detect occupied port", async () => {
+			
+			await vscode.commands.executeCommand("atlasmap.start");
+			const detect = require('detect-port');
+			const co = require('co');
+			await( (await co(function *() {
+				const _port = yield detect(port);
+				return port == _port;
+			})) == true);
+
+			await vscode.commands.executeCommand("atlasmap.start");
+			assert.ok(errorMessageSpy.calledOnceWithExactly("The port " + port + " is already occupied. Choose a different port.", sinon.match.any));
+			
 		});
 	});
 });
